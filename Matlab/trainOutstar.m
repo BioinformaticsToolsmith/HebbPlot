9618f075505ba67a1956a0ede09f13bcf9a16a79
HebbPlot <34320728+HebbPlot@users.noreply.github.com>
2018-04-15 10:07:26 -0500

    Update trainOutstar.m

--- Matlab/trainOutstar.m
+++ Matlab/trainOutstar.m
@@ -11,16 +11,10 @@ function w = trainOutstar( p0, p, alpha )
 
 w0 = ones(1, size(p0,2));
 w = zeros(1, size(p0,2));
-% convergenceList = zeros(1, size(p0, 1));
 
 for i = 1:size(p0, 1)
-% wOld = w;
 a = satlins((p0(i, :) .* w0) + (p(i,:) .* w) );
 w = w + alpha * (a - w) .* p(i, :);
-% convergenceList(i) = dotSim(w, wOld);
 end
-% plot(convergenceList(1:500));
-% print ('/Users/alfredo/Writing/HebbPlot/Supplementary/Convergence/Promoters_E003.pdf', '-dpdf');
-% error('nothing')
 end
 
